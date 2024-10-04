863b42ce78591863246b01d2da6dbd2ae55be936
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-10-04 10:37:14 +0900

    design: footer ui
    - #17

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Footer from "@components/layouts/Footer";
 
 const MainLayout = () => (
   <div className="main-layout">
     <Outlet />
+    <Footer />
   </div>
 );
 
