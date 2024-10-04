842e7ee123afe7f4ae833209414b4c4182a00cd6
rudtj <kyoungseo03@naver.com>
2024-10-04 22:40:34 +0900

    feat: IntroductionPage 구현

---
+++ src/pages/Introduction/index.tsx
@@ -0,0 +1,11 @@
+import Introduction from "../../components/features/IntroductionPage/Introduction";
+import Header from "../../components/layouts/Header";
+
+const IntroductionPage = () => (
+  <>
+    <Header />
+    <Introduction />
+  </>
+);
+
+export default IntroductionPage;
