39cd1f8d648330e1863e5d68249c7a63df0a391a
gimdogyun <jasper12394@gmail.com>
2024-10-12 00:25:21 +0900

    feat: remove maxItem
    - #31

--- src/components/ItemView/ListView.tsx
+++ src/components/ItemView/ListView.tsx
@@ -2,20 +2,14 @@ import { ViewProps } from "@components/ItemView/type";
 import Card, { CardProps } from "@components/common/Card";
 import type { Item } from "@type";
 
-export type ListViewProps<T extends Item> = ViewProps<T> & {
-  maxItems: number;
-} & CardProps;
+export type ListViewProps<T extends Item> = ViewProps<T> & CardProps;
 
-const ListView = <T extends Item>({ items, ItemComponent, maxItems = 10, ...props }: ListViewProps<T>) => {
-  const itemsToShow = items.slice(0, maxItems);
-
-  return (
-    <Card {...props}>
-      {itemsToShow.map(item => (
-        <ItemComponent key={item.id} item={item} />
-      ))}
-    </Card>
-  );
-};
+const ListView = <T extends Item>({ items, ItemComponent, ...props }: ListViewProps<T>) => (
+  <Card {...props}>
+    {items.map(item => (
+      <ItemComponent key={item.id} item={item} />
+    ))}
+  </Card>
+);
 
 export default ListView;
