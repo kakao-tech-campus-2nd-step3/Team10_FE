3c79d3dd477564316d970e692eafc73d2bb1aceb
gimdogyun <jasper12394@gmail.com>
2024-10-12 00:20:01 +0900

    refactor: move title to card
    - #30

--- src/components/ItemView/ListView.tsx
+++ src/components/ItemView/ListView.tsx
@@ -1,24 +1,16 @@
-import { Divider, Text } from "@chakra-ui/react";
 import { ViewProps } from "@components/ItemView/BaseView";
 import Card, { CardProps } from "@components/common/Card";
 import type { Item } from "@type";
 
 export type ListViewProps<T> = ViewProps<T> & {
   maxItems: number;
-  title?: string;
 } & CardProps;
 
-const ListView = <T extends Item>({ items, ItemComponent, maxItems = 10, title, ...props }: ListViewProps<T>) => {
+const ListView = <T extends Item>({ items, ItemComponent, maxItems = 10, ...props }: ListViewProps<T>) => {
   const itemsToShow = items.slice(0, maxItems);
 
   return (
     <Card {...props}>
-      {title && [
-        <Text color="#22543D" fontSize="x-large" fontWeight="bold">
-          {title}
-        </Text>,
-        <Divider mt="2" opacity="1" borderWidth="2px" borderColor="#22543D" />,
-      ]}
       {itemsToShow.map(item => (
         <ItemComponent key={item.id} item={item} />
       ))}
--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,9 +1,17 @@
-import { Flex, type FlexProps } from "@chakra-ui/react";
+import { Divider, Flex, Text, type FlexProps } from "@chakra-ui/react";
 
-export type CardProps = FlexProps;
+export type CardProps = FlexProps & {
+  title?: string;
+};
 
-const Card = (props: CardProps) => (
+const Card = ({ title, ...props }: CardProps) => (
   <Flex direction="column" bg="white" borderRadius="10" {...props}>
+    {title && [
+      <Text color="#22543D" fontSize="x-large" fontWeight="bold">
+        {title}
+      </Text>,
+      <Divider mt="2" opacity="1" borderWidth="2px" borderColor="#22543D" />,
+    ]}
     {props?.children}
   </Flex>
 );
