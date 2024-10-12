0133018b33535aca764ab0bfc8b167b5a2f98bd7
gimdogyun <jasper12394@gmail.com>
2024-10-12 23:00:07 +0900

    feat: type and mock
    - #29

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,19 @@
 export type Item = {
   id: number;
 };
+
+export type Farm = Item & {
+  name: string;
+  address: string;
+  profile?: string;
+};
+
+export type Product = Item & {
+  name: string;
+  unit: string;
+  pricePerGram: string;
+  description: string;
+  mainImage?: string;
+  tag?: string;
+  farm: Farm;
+};
