f87be12ff6421cf5daa6ca65528076d21815fbff
rudtj <kyoungseo03@naver.com>
2024-10-16 01:15:57 +0900

    feat: FarmDetailPage 구현

---
+++ src/pages/FarmDetailPage/index.tsx
@@ -0,0 +1,15 @@
+import Direction from "@components/features/FarmDetailPage/Direciton";
+import Product from "@components/features/FarmDetailPage/Product";
+import ProductDescription from "@components/features/FarmDetailPage/ProductDescription";
+import Reviews from "@components/features/FarmDetailPage/Reviews";
+
+const FarmDetailPage = () => (
+  <>
+    <Product />
+    <ProductDescription />
+    <Reviews />
+    <Direction />
+  </>
+);
+
+export default FarmDetailPage;
