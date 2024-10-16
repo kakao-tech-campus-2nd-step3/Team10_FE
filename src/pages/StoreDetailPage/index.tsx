15346e446c553809bf48b4163d95a8a0506e4102
rudtj <kyoungseo03@naver.com>
2024-10-16 18:07:44 +0900

    feat: StoreDetailPage 구현

---
+++ src/pages/StoreDetailPage/index.tsx
@@ -0,0 +1,15 @@
+import Direction from "@components/features/StoreDetailPage/Direction";
+import Product from "@components/features/StoreDetailPage/Product";
+import ProductDescription from "@components/features/StoreDetailPage/ProductDescription";
+import Reviews from "@components/features/StoreDetailPage/Reviews";
+
+const StoreDetailPage = () => (
+  <>
+    <Product />
+    <ProductDescription />
+    <Reviews />
+    <Direction />
+  </>
+);
+
+export default StoreDetailPage;
