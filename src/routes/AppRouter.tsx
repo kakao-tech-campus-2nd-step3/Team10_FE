c69749397fccefc55e282e1a80c82f30e9fd4507
rudtj <kyoungseo03@naver.com>
2024-11-06 20:36:51 +0900

    remove: AppRouter 상태 제거

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -7,39 +7,41 @@ import IntroductionPage from "@pages/Introduction";
 import SellerMyPage from "@pages/SellerMyPage";
 import StoreDetailPage from "@pages/StoreDetailPage";
 
-const routers = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "introduction",
-        element: <IntroductionPage />,
-      },
-      {
-        path: "BuyerMyPage",
-        element: <BuyerMyPage />,
-      },
-      {
-        path: "SellerMyPage",
-        element: <SellerMyPage />,
-      },
-      {
-        path: "StoreDetail",
-        element: <StoreDetailPage />,
-      },
-      {
-        path: "FarmDetail",
-        element: <FarmDetailPage />,
-      },
-    ],
-  },
-]);
+const AppRouter = () => {
+  const routers = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "introduction",
+          element: <IntroductionPage />,
+        },
+        {
+          path: "BuyerMyPage",
+          element: <BuyerMyPage />,
+        },
+        {
+          path: "SellerMyPage",
+          element: <SellerMyPage />,
+        },
+        {
+          path: "StoreDetail",
+          element: <StoreDetailPage />,
+        },
+        {
+          path: "FarmDetail",
+          element: <FarmDetailPage />,
+        },
+      ],
+    },
+  ]);
 
-const AppRouter = () => <RouterProvider router={routers} />;
+  return <RouterProvider router={routers} />;
+};
 
 export default AppRouter;
