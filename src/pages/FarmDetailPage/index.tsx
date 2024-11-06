4e0ff27018905779462c089309c14f1ad917e51f
rudtj <kyoungseo03@naver.com>
2024-11-07 01:22:01 +0900

    refactor: tabs 추가

--- src/pages/FarmDetailPage/index.tsx
+++ src/pages/FarmDetailPage/index.tsx
@@ -1,6 +1,7 @@
-import Direction from "@components/features/FarmDetailPage/Direciton";
+import { Tab, Tabs, TabList, TabPanels, TabPanel } from "@chakra-ui/react";
 import Product from "@components/features/FarmDetailPage/Product";
 import ProductDescription from "@components/features/FarmDetailPage/ProductDescription";
+import ReviewList from "@components/features/FarmDetailPage/ReviewList";
 import Reviews from "@components/features/FarmDetailPage/Reviews";
 
 const FarmDetailPage = () => (
@@ -8,7 +9,21 @@ const FarmDetailPage = () => (
     <Product />
     <ProductDescription />
     <Reviews />
-    <Direction />
+    <Tabs mt={100} ml={200}>
+      <TabList w="1100px">
+        <Tab _selected={{ color: "#1C4532", fontSize: "24px", fontWeight: "bold", bg: "none" }}>상품 설명</Tab>
+        <Tab _selected={{ color: "#1C4532", fontSize: "24px", fontWeight: "bold", bg: "none" }}>후기</Tab>
+      </TabList>
+
+      <TabPanels py="10px">
+        <TabPanel>
+          <ProductDescription />
+        </TabPanel>
+        <TabPanel>
+          <ReviewList />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   </>
 );
 
