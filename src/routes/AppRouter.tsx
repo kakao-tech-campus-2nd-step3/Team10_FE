0c784831fb3330bf234d28eb96ac5394dc2bb66c
rudtj <kyoungseo03@naver.com>
2024-11-09 21:38:02 +0900

    feat: 페이지 경로 추가

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "@components/layouts/MainLayout";
 import BuyerMyPage from "@pages/BuyerMyPage";
+import EmailLoginPage from "@pages/EmailLoginPage";
+import EmailRegisterPage from "@pages/EmailRegisterPage";
 import FarmDetailPage from "@pages/FarmDetailPage";
+
 import Home from "@pages/Home";
 import IntroductionPage from "@pages/Introduction";
 import MyPage from "@pages/MyPage";
+import RegisterPage from "@pages/RegisterPage";
 import SellerMyPage from "@pages/SellerMyPage";
 import SellerPage from "@pages/SellerPage";
 import StoreDetailPage from "@pages/StoreDetailPage";
@@ -46,6 +50,18 @@ const AppRouter = () => {
         {
           path: "MyPage",
           element: <MyPage />,
+        },
+        {
+          path: "Register",
+          element: <RegisterPage />,
+        },
+        {
+          path: "EmailLogin",
+          element: <EmailLoginPage />,
+        },
+        {
+          path: "EmailRegister",
+          element: <EmailRegisterPage />,
         },
       ],
     },
