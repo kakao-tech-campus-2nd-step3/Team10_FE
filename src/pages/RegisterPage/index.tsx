790150154ca799006b7b389ea1b69514a9c411ce
gimdogyun <jasper12394@gmail.com>
2024-11-14 04:28:18 +0900

    feat: RegisterBackground
    - #106

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,25 +1,10 @@
-import { Box, Flex } from "@chakra-ui/react";
-import register from "@assets/Image/Intro/Register.png";
 import Register from "@components/features/RegisterPage/Register";
+import RegisterBackground from "@components/features/RegisterPage/RegisterBackground";
 
 const RegisterPage = () => (
-  <Flex pos="relative" align="center" justify="center" w="100vw" h="100vh">
-    <Box
-      pos="absolute"
-      zIndex={0}
-      top={0}
-      left={0}
-      w="100%"
-      h="100%"
-      bgImage={`url(${register})`}
-      bgSize="cover"
-      bgPosition="center"
-    />
-    <Box pos="absolute" zIndex={1} top={0} right={0} bottom={0} left={0} bgColor="rgba(0, 0, 0, 0.5)" />
-    <Box zIndex={3}>
-      <Register />
-    </Box>
-  </Flex>
+  <RegisterBackground>
+    <Register />
+  </RegisterBackground>
 );
 
 export default RegisterPage;
