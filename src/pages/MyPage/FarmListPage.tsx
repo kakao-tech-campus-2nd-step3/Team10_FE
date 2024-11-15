665cbb8452ecf7ae053a88b30febc64cfdca5125
rudtj <kyoungseo03@naver.com>
2024-11-15 16:05:22 +0900

    feat: 농장 체험 페이지 구현

---
+++ src/pages/MyPage/FarmListPage.tsx
@@ -0,0 +1,10 @@
+import Card from "@components/common/Card";
+import FarmList from "@components/features/MyPage/Order/FarmList";
+
+const FarmListPage = () => (
+  <Card title="농장 체험 내역" w="100%" h="fit-content" p="10">
+    <FarmList />
+  </Card>
+);
+
+export default FarmListPage;
