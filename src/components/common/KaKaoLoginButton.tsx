e505c4d839936a0862ec034a39415f2523d84e98
gimdogyun <jasper12394@gmail.com>
2024-11-18 21:59:24 +0900

    test: localtest
    - #140

--- src/components/common/KaKaoLoginButton.tsx
+++ src/components/common/KaKaoLoginButton.tsx
@@ -4,7 +4,7 @@ import Image from "@components/common/Image";
 type KaKaoLoginButtonProps = ImageProps;
 
 const KaKaoLoginButton = ({ ...props }: KaKaoLoginButtonProps) => {
-  const link = "https://api.poomasi.shop/oauth2/authentication/kakao";
+  const link = "http://172.22.42.12:8080/oauth2/authentication/kakao";
 
   return (
     <Link href={link} isExternal>
