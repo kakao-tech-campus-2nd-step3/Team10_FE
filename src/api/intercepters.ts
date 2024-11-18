3dcaecf0ce4510b987867487d5d926178bd3c52d
gimdogyun <jasper12394@gmail.com>
2024-11-18 18:36:25 +0900

    refactor: change localstorage key
    - #140

--- src/api/intercepters.ts
+++ src/api/intercepters.ts
@@ -13,10 +13,10 @@ const refrechIntercepter = async (response: AxiosResponse): Promise<AxiosRespons
       alert("로그아웃 되었습니다. 다시 로그인해주세요.");
       window.location.href = "/";
     });
-    const user = JSON.parse(localStorage.getItem("poomasi_user") || "{}");
+    const user = JSON.parse(localStorage.getItem("poomasi-user") || "{}");
 
     localStorage.setItem(
-      "poomasi_user",
+      "poomasi-user",
       JSON.stringify({
         ...user,
         token: result,
