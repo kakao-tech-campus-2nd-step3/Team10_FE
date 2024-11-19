5e6b5876a90e2fb8b19aae2dbbf51379c43fd287
gimdogyun <jasper12394@gmail.com>
2024-11-19 21:37:28 +0900

    fix: redirect after login
    - #145

--- src/pages/CallbackPage/index.tsx
+++ src/pages/CallbackPage/index.tsx
@@ -1,14 +1,17 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import useLogin from "@hooks/useLogin";
 
 const CallbackPage = () => {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const { login } = useLogin();
 
   const accessToken = searchParams.get("access");
 
   if (accessToken) {
-    login(accessToken);
+    login(accessToken).then(() => {
+      navigate("/");
+    });
   }
 
   return null;
