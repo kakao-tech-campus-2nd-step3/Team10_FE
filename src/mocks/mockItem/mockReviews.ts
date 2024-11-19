0527c958e91ae5139ca89ed2ba7614db289207b9
rudtj <kyoungseo03@naver.com>
2024-11-19 20:25:59 +0900

    feat: mock review 추가

---
+++ src/mocks/mockItem/mockReviews.ts
@@ -0,0 +1,52 @@
+const mockReviews = [
+  {
+    id: 1,
+    name: "건호네 미나리",
+    image: "/assets/image/fresh_veggies.png",
+    description: "신선하고 맛있는 미나리! 무농약으로 재배되어 믿고 먹을 수 있어요.",
+    rating: 5,
+    date: "2024-09-14",
+  },
+  {
+    id: 2,
+    name: "토맛 토마토",
+    image: "/assets/image/fresh_veggies.png",
+    description: "달콤하고 신선한 토마토, 정말 맛있어요! 샐러드에 넣어 먹으면 최고입니다.",
+    rating: 4,
+    date: "2024-09-14",
+  },
+  {
+    id: 3,
+    name: "깻잎장수 지민",
+    image: "/assets/image/fresh_veggies.png",
+    description: "향긋한 깻잎, 엄마의 손맛이 느껴져요. 쌈으로 먹으면 더욱 맛있습니다.",
+    rating: 5,
+    date: "2024-09-24",
+  },
+  {
+    id: 4,
+    name: "할로윈 늙은 호박",
+    image: "/assets/image/fresh_veggies.png",
+    description: "늙은 호박으로 만든 전이 너무 맛있어요. 할로윈에 꼭 추천합니다!",
+    rating: 5,
+    date: "2024-09-04",
+  },
+  {
+    id: 5,
+    name: "몸에 좋은척 하는 기이이이이이이이이이이이인 가지",
+    image: "/assets/image/fresh_veggies.png",
+    description: "가지의 다양한 요리를 시도해보세요. 건강에도 좋고 맛도 좋습니다.",
+    rating: 5,
+    date: "2024-09-14",
+  },
+  {
+    id: 6,
+    name: "집에 가고싶을 땐 복숭아를",
+    image: "/assets/image/fresh_veggies.png",
+    description: "달콤한 복숭아 아이스티로 더위를 날려보세요. 기분이 좋아지는 맛입니다.",
+    rating: 4.5,
+    date: "2024-09-21",
+  },
+];
+
+export default mockReviews;
