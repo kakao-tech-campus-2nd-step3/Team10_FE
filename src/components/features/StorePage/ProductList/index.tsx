bb8527ddbf9642132be4c3c77c33995d8ceb3a22
gimdogyun <jasper12394@gmail.com>
2024-11-19 21:36:15 +0900

    design: Product
    - #145

--- src/components/features/StorePage/ProductList/index.tsx
+++ src/components/features/StorePage/ProductList/index.tsx
@@ -12,7 +12,7 @@ const ProductList = () => {
     <Flex gap="5" w="100%" h="100vh">
       <ProductFilter filterState={productFilterState} items={mockCategory.map(encodeCategory)} />
       <Flex overflow="scroll" w="100%" h="100%">
-        <ProductsView filters={productFilterState.filters} columns={3} gap="25px" />
+        <ProductsView filters={productFilterState.filters} py="10" columns={3} gap="25px" />
       </Flex>
     </Flex>
   );
